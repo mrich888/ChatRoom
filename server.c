c542536dadb71e4e129a4730ecd33f35cd83b8ac
yywjk-dmn <2986567641@qq.com>
2024-01-19 21:35:34 +0800

    [新增]又写了一个测试函数

---
+++ server.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+#define SIZE 10
+
+int main()
+{
+    int a = SIZE;
+    if (a > 0)
+    {
+        printf("is a 正数");
+    }
+    else
+    {
+        printf("是个负数");
+    }
+
+
+
+    return 0;
+}
