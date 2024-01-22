635562ffeffd38737f3708314970666e9371997a
yywjk-dmn <2986567641@qq.com>
2024-01-22 21:43:39 +0800

    [新增]新增线程池的代码

---
+++ threadPool.h
@@ -0,0 +1,22 @@
+#ifndef __THREAD_POOL_H
+#define __THREAD_POOL_H
+
+
+
+
+
+
+
+/* 线程池的初始化 */
+int threadPoolInit();
+
+/* 线程池添加任务 */
+int threadPoolAddTask();
+
+/* 线程池的销毁 */
+int threadPoolDestory();
+
+
+
+
+#endif
